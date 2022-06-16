a3ed41282dc59870b1f1a31b0dfad4e46080e6a7
Olga-Rabushko1983 <oiushka1@mail.ru>
2022-06-16 19:58:52 +0300

    add class Task01

---
+++ src/by/itstep/OlgaRabushko/javalessons/lesson18/controller/Task01.java
@@ -0,0 +1,20 @@
+package by.itstep.OlgaRabushko.javalessons.lesson18.controller;
+
+import by.itstep.OlgaRabushko.javalessons.lesson18.view.Printer;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+        do {
+            Printer.print("Input size of array");
+            size = scanner.nextInt();
+        }
+        while (size <= 0);
+        int[] array = new int[size];
+
+
+    }
+}
