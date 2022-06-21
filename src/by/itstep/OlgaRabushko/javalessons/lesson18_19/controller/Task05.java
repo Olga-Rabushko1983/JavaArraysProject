479d2d98973eba912aa33b6c87d74b32891089a8
Olga-Rabushko1983 <oiushka1@mail.ru>
2022-06-21 21:41:36 +0300

    add buble sorter

---
+++ src/by/itstep/OlgaRabushko/javalessons/lesson18_19/controller/Task05.java
@@ -0,0 +1,32 @@
+package by.itstep.OlgaRabushko.javalessons.lesson18_19.controller;
+
+import by.itstep.OlgaRabushko.javalessons.lesson18_19.modelLogic.ArraySorter;
+import by.itstep.OlgaRabushko.javalessons.lesson18_19.modelLogic.ArrayWorker;
+import by.itstep.OlgaRabushko.javalessons.lesson18_19.util.ArrayInitializer;
+import by.itstep.OlgaRabushko.javalessons.lesson18_19.util.Convertor;
+import by.itstep.OlgaRabushko.javalessons.lesson18_19.view.Printer;
+
+import java.util.Scanner;
+
+public class Task05 { public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int size;
+    do {
+        Printer.print("Input size of array");
+        size = scanner.nextInt();
+    }
+    while (size <= 0);
+    int[] array = new int[size];
+    //ArrayInitializer.userInit(array);
+    ArrayInitializer.randomeInt(array);
+
+    Printer.print("Array before sort:"+ Convertor.toString(array));
+    ArraySorter.bubbleSortAcs(array);
+    Printer.print("\nArray after sort:"+Convertor.toString(array));
+
+    ArrayWorker.reverse(array);
+    Printer.print("\nArray after reverse:"+Convertor.toString(array));
+}
+}
+
